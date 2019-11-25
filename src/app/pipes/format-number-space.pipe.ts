c6f1008b01445573622edf336d8750ba7dd8a855
ychafia <chafiayouness@gmail.com>
2019-11-25 11:46:03 +0100

    fix bug for dicimal number

--- src/app/pipes/format-number-space.pipe.ts
+++ src/app/pipes/format-number-space.pipe.ts
@@ -6,7 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FormatNumberSpacePipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
-    if(value != undefined) {
+    if(value != undefined && !isNaN(value)) {
+      value = value.toFixed(2);
       let parts = value.toString().split(".");
       parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
       return parts.join(".");
