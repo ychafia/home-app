94561d2d67b4438c56adda08e45ff7684b022e30
unknown <chafiayouness@gmail.com>
2019-12-16 15:52:04 +0100

    update index (angular material)

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,7 +1,7 @@
 export class ConfigAPI {
     // public static API_ENDPOINT='http://192.168.1.32:8080/home-app-api'; //Deployed in tomcat in home-app-api folder
     // public static API_ENDPOINT='http://localhost:8080'; //local MSA-Project
-    // public static API_ENDPOINT='http://localhost:8080';
+    public static API_ENDPOINT='http://localhost:8080';
     // public static API_ENDPOINT='http://192.168.1.32:8080'; // prod without tomcat
-    public static API_ENDPOINT='http://77.202.38.213:8080'; // prod without tomcat
+    // public static API_ENDPOINT='http://77.202.38.213:8080'; // prod without tomcat
 }
